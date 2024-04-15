bcd9e8b7eedfcdfe56ef8ba237a1ca7ed2e5d6a3
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-04-15 16:45:45 +0100

    creating a new event

--- Client/src/Components/client/UserDashboard.jsx
+++ Client/src/Components/client/UserDashboard.jsx
@@ -1,12 +1,19 @@
 import { Button } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import React from "react";
 
 const UserDashboard = () => {
   return (
     <>
-      <section className="bg-gray-100 px-6">
-        <main className="bg-white px-5 py-10 lg:w-full">
-           <Button>Create event</Button>
+      <section className="px-2 h-screen">
+        <main className="bg-white px-5 py-10 lg:w-full flex justify-center">
+           <Button variant="contained" className="poppins-medium-sm">Create event <AddIcon className="ml-2 "/></Button>
+        </main>
+        <main>
+          <h6 className="text-center poppins-medium-sm">EVENT HISTORY</h6>
+         <div className="shadow-md w-20">
+
+         </div>
         </main>
       </section>
     </>
